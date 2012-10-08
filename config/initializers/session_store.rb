305002ecbe5673f44a0b3504c54f84242773adfa
rmosolgo <rdmosolgo@gmail.com>
2012-10-08 10:08:23 -0400

    Initial Commit

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Adt::Application.config.session_store :cookie_store, key: '_adt_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Adt::Application.config.session_store :active_record_store
