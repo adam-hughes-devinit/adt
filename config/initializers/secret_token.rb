54f9e5027bf93b78387563dedba69195056c8b0a
rmosolgo <rdmosolgo@gmail.com>
2012-11-13 17:04:03 -0500

    updated Readme

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Adt::Application.config.secret_token = '9788ea2e94750ba311f8ba36ae3dcdb3fbf2779cc5e8abb0a6c7181ef186eb03b1ebc7d10a5053017526a676b6453f6eae76430154dc4b1e0a203b77c63305ba'
+Adt::Application.config.secret_token = 'z788ea2e94750ba311f8ba36ae3dcdb3fbf2779cc5e8abb0a6c7181ef186eb03b1ebc7d10a5053017526a676b6453f6eae76430154dc4b1e0a203b77c63305ba'
