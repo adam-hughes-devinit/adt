1d3e65eadd1796c81ccd06410543148fe1b22f75
rmosolgo <rdmosolgo@gmail.com>
2012-11-13 14:25:14 -0500

    Basic auth in production env

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,9 @@ class ApplicationController < ActionController::Base
   before_filter :signed_in_user, except: [:index, :show]
   protect_from_forgery
   include SessionsHelper
+  if Rails.env.production?
+  	http_basic_authenticate_with name:'aiddata_china', password: 'a1dd4t4'
+  end
+
 
 end
