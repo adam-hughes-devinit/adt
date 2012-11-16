4cd23f94219e311f0b2a3d5cae6c8288eb8e06cf
rmosolgo <rdmosolgo@gmail.com>
2012-11-16 18:26:49 -0500

    fix endless loop in caching process

--- app/models/cache.rb
+++ app/models/cache.rb
@@ -3,7 +3,8 @@ class Cache < ActiveRecord::Base
   after_save :update_complete_cache
   
   def update_complete_cache
+  
   	Cache.find_or_create_by_id(0).update_attribute(:text, Cache.all.map{|c| c.text}.join("
-"))
+")) unless id==0
 	end
 end
