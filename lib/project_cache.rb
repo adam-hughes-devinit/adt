c3797da0711f4b990aec82abd310fd565f9c2a54
Adam Frey <adamparkerfrey@gmail.com>
2013-02-05 14:27:16 -0500

    backgrounds cache process

--- lib/project_cache.rb
+++ lib/project_cache.rb
@@ -5,15 +5,17 @@ module ProjectCache
 		if CACHE_ONE
 			cached_record = Cache.find_or_create_by_id(id)
 			cached_record.skip_cache_all = false
-			cached_record.update_attribute(:text, self.to_csv)
+			cached_record.update_attribute(:text, self.csv_text)
 		end
 	end
+	handle_asynchronously :cache!
 	
 	def cache_one!
 		if CACHE_ONE
 			cached_record = Cache.find_or_create_by_id(id)
 			cached_record.skip_cache_all = true
-			cached_record.update_attribute(:text, self.to_csv)
+			cached_record.update_attribute(:text, self.csv_text)
 		end
 	end
+	handle_asynchronously :cache_one!
 end
