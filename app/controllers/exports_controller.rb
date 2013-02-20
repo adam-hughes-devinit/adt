b8c9213a7d4dc50c1740760ea299487f1e6429f1
Adam Frey <adamparkerfrey@gmail.com>
2013-02-20 16:01:47 -0500

    Adds basic export functionality

--- app/controllers/exports_controller.rb
+++ app/controllers/exports_controller.rb
@@ -14,6 +14,15 @@ class ExportsController < ApplicationController
   end
 
   def create
+    projects_string = params[:export][:projects]
+    projects_string.gsub!(/[\[\]]/,'')
+    projects_array = []
+    projects_string.split(",").map do |proj_id|
+      proj_id.strip!
+      projects_array << Project.find(proj_id.to_i)
+    end
+    params[:export][:projects] = projects_array
+
     @export = Export.new(params[:export])
     if @export.save
       redirect_to(@export, notice: 'Post was successfully created.')
