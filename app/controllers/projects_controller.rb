bce5c2302ea8000631c179359a86167e3122822d
rmosolgo <rdmosolgo@gmail.com>
2013-03-25 14:36:26 -0400

    repair caching

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -3,8 +3,8 @@ before_filter :set_owner, only: [:create, :new]
 before_filter :correct_owner?, only: [:edit]
 include SearchHelper
 
-caches_page :show, :cache_path => Proc.new { |c| "#{current_user_is_aiddata}/projects/#{c.params[:id]}"}
-caches_page :index, :cache_path => Proc.new { |c| "#{current_user_is_aiddata}/projects/#{c.params.inspect}" }
+caches_page :show, :cache_path => Proc.new { |c| "projects/#{c.params[:id]}_#{current_user_is_aiddata}"}
+caches_page :index, :cache_path => Proc.new { |c| "projects/#{c.params.inspect}_#{current_user_is_aiddata}" }
 
   def index
    
