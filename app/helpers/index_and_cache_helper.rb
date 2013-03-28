da2c2b6c2676b9a051056aa66d1790585f983c4d
rmosolgo <rdmosolgo@gmail.com>
2013-03-28 09:19:30 -0400

    Change caching when editing a code

--- app/helpers/index_and_cache_helper.rb
+++ app/helpers/index_and_cache_helper.rb
@@ -10,17 +10,17 @@ module IndexAndCacheHelper
 
 	def reindex_projects(projects)
 		p "Reindex projects at #{Time.new}"
-		Sunspot.index(projects || Project.all)
-		Sunspot.commit
+		Sunspot.delay.index!(projects || Project.all)
+		# Sunspot.commit
 	end
-	handle_asynchronously :reindex_projects
+	# handle_asynchronously :reindex_projects
 
 	def recache_projects(projects)
 		p "Recache projects at #{Time.new}"
 		projects = projects || Project.all
-		projects.each{ |p| p.cache! now: true, include_files: false }
+		projects.each{ |p| p.delay.cache! now: true, include_files: false }
 	end
-	handle_asynchronously :recache_projects
+	# handle_asynchronously :recache_projects
 
 	def remake_files
 		Scope.all.each do |s|
@@ -28,7 +28,8 @@ module IndexAndCacheHelper
 	 		Project.all.sample.cache_files(s.symbol.to_sym)
 	 	end
 	end
-	handle_asynchronously :remake_files
+	
+	# handle_asynchronously :remake_files
 	
 
   def reindex_and_recache_by_associated_object(object)
@@ -39,4 +40,5 @@ module IndexAndCacheHelper
       remake_files	
     end	
   end
+
 end
