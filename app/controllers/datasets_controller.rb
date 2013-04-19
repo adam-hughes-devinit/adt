eaff19443bc01d0076dc41a7ad7056d32317cde0
rmosolgo <rdmosolgo@gmail.com>
2013-04-19 10:35:21 -0400

    static files partial

--- app/controllers/datasets_controller.rb
+++ app/controllers/datasets_controller.rb
@@ -1,10 +1,11 @@
 class DatasetsController < ApplicationController
 
 	def index
-		redirect_to( Content.find_by_name("downloads") || "/")
+		redirect_to( content_by_name_path("downloads") || "/")
 	end
 
 	def show
+		# dataset must be named "aiddata_china_x_y.xlsx" where x and y are version major/minor, ie 1.0 x=1, y=0
 		this_dataset_location = Rails.root.join('public', 'static_datasets') + "aiddata_china_#{params[:id].gsub(/\./, "_")}.xlsx"
 		send_file this_dataset_location
 	end
