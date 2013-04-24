b3e2f6d5e09d0824515518fa0c93ae89eac9d3ea
Adam Frey <adamparkerfrey@gmail.com>
2013-04-24 14:19:56 -0400

    updated Procfile for heroku

--- config/application.rb
+++ config/application.rb
@@ -69,6 +69,10 @@ module Adt
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    
+    # Action mailer settings for Heroku
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.raise_delivery_errors = true
 
   end
 end
--- config/environment.rb
+++ config/environment.rb
@@ -1,9 +1,6 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
-# Action mailer settings for Heroku
-config.action_mailer.delivery_method = :smtp
-config.action_mailer.raise_delivery_errors = true
 
 # Initialize the rails application
 Adt::Application.initialize!
