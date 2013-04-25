9af51dfbed7f31db09e93041c17facc93615421d
Adam Frey <adamparkerfrey@gmail.com>
2013-04-25 16:35:14 -0400

    begins upload to s3 functionality

--- app/helpers/amazon_helper.rb
+++ app/helpers/amazon_helper.rb
@@ -1,17 +1,17 @@
 module AmazonHelper
-  def upload(tempfile, filename)
+  def s3_upload(bucket, tempfile, filename)
     # tempfile is a Tempfile
     # filename is the name it should be stored as (in my case, MD5)
 
     s3 = AWS::S3.new(
-        :access_key_id => AWS_ACCESS_KEY_ID, 
-        :secret_access_key => AWS_ACCESS_SECRET_KEY 
+        :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+        :secret_access_key => ENV['AWS_ACCESS_SECRET_KEY'] 
     )
-    p "Uploading file to S3 #{EXPORT_BUCKET}"
+    p "Uploading file to S3 #{bucket}"
 
     # just in case 
     tempfile.rewind
-    obj = s3.buckets[EXPORT_BUCKET].objects[filename].write(tempfile.read)
+    obj = s3.buckets[bucket].objects[filename].write(tempfile.read)
 
     # Oh heck, make sure people can download this stuff.
 
