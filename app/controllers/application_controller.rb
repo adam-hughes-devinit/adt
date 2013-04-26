8ae434c9d989e6e6562db06adc8b4dae9004cced
rmosolgo <rdmosolgo@gmail.com>
2013-04-26 19:02:12 -0400

    Force proper domain

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   before_filter :signed_in_user, except: [:index, :show]
   before_filter :mailer_set_url_options
+  before_filter :force_proper_domain
   
   protect_from_forgery
   
@@ -8,6 +9,16 @@ class ApplicationController < ActionController::Base
   
   if Rails.env.production?
   	http_basic_authenticate_with name:'aiddata_china', password: 'a1dd4t4'
+  end
+
+  def force_proper_domain
+    if Rails.env.production?
+      if (params[:host] !~ /china\.aiddata\.org/) && (params[:host] !~ /aiddatachina/)
+        params[:host] = 'china.aiddata.org'
+        params[:port] = nil
+        redirect_to url_for(params), :status => 301 
+      end
+    end
   end
 
   def mailer_set_url_options
