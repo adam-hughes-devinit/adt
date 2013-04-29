a3ed9f8596764cc1c7f358162f69a52a5853a730
rmosolgo <rdmosolgo@gmail.com>
2013-04-29 12:54:33 -0400

    Stub out initializer :(

--- config/initializers/z_load_project_csv.rb
+++ config/initializers/z_load_project_csv.rb
@@ -1,4 +1,4 @@
-# Pre-fill csv text in production
-if Rails.env.production?
-	Project.find_each(&:csv_text)
-end
+# # Pre-fill csv text in production
+# if Rails.env.production?
+# 	Project.find_each(&:csv_text)
+# end
