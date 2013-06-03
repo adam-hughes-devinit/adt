6f2511b2b862e8c3eb1d29ff4220fbe8a0465cf8
rmosolgo <rdmosolgo@gmail.com>
2013-06-03 08:41:34 -0400

    More moving to search module, add suggest link to header

--- app/models/project.rb
+++ app/models/project.rb
@@ -12,7 +12,6 @@ class Project < ActiveRecord::Base
     :is_cofinanced,
     # belongs_to fields
     :status, :verified, 
-    ## :tied, :tied_id, -- removed so that it will break if it's used somewhere. 
     :flow_type, :oda_like, :sector,
     #convoluted fields
     :donor, :owner, 
@@ -360,58 +359,15 @@ class Project < ActiveRecord::Base
   belongs_to :owner, class_name: "Organization"
   delegate :name, to: :owner, allow_nil: true, prefix: true
 
-  # for filtering
-  def active_string 
-    active? ? 'Active' : 'Inactive'
-  end
 
-  def is_commercial_string
-    is_commercial? ? 'Commercial' : 'Not Commercial'
-  end
-
-  def year_uncertain_string
-    year_uncertain? ? "Year Uncertain" : "Year Certain"
-  end
-
-  def debt_uncertain_string
-    debt_uncertain ? "Debt Relief Uncertain" : "Not Uncertain"
-  end
-
-  def line_of_credit_string
-    line_of_credit ? "Line of Credit" : "Not Line of Credit"
-  end
-
-  def is_cofinanced_string
-    is_cofinanced ? "Cofinanced" : "Not Cofinanced"
-  end
 
   # project accessories
   has_many :geopoliticals, dependent: :destroy
   accepts_nested_attributes_for :geopoliticals, allow_destroy: true, :reject_if => proc { |a| a['recipient_id'].blank? }
-  def country_name
-    geopoliticals.map do |g|
-      g.recipient ? g.recipient.name : 'Unset'
-    end.sort
-  end
-  def recipient_condensed
-    country_name.count > 1 ? "Africa, regional" : country_name[0]
-  end
-  def number_of_recipients
-    country_name.length
-  end
+
 
   has_many :transactions, dependent: :destroy
   accepts_nested_attributes_for :transactions, allow_destroy: true, :reject_if => proc { |a| a['value'].blank? }
-  def usd_2009
-    sum = 0
-    transactions.map { |t| sum += t.usd_defl unless t.usd_defl.nil?} 
-    sum > 0 ? sum : nil
-  end
-  def currency_name
-    transactions.map do |t| 
-     t.currency_name || 'Unset'
-    end
-  end
 
 
   has_many :contacts, dependent: :destroy  
@@ -419,36 +375,10 @@ class Project < ActiveRecord::Base
 
   has_many :sources, dependent: :destroy
   accepts_nested_attributes_for :sources, allow_destroy: true, :reject_if => proc { |a| a['url'].blank? }
-  def document_type_name
-    d = sources.map {|s| s.document_type_name || 'Unset'}
-  end
-
-  def source_type_name
-    s = sources.map {|s| s.source_type_name || 'Unset'}
-  end
-
-  def search_engine_type_name
-    # alias for search interface & URLs
-    source_type_name
-  end
 
   has_many :participating_organizations, dependent: :destroy
   accepts_nested_attributes_for :participating_organizations, allow_destroy: true, :reject_if => proc { |a| a['organization_id'].blank? }
-  def origin_name
-    participating_organizations.map {|p| p.origin_name || 'Unset'}
-  end
 
-  def organization_type_name
-    participating_organizations.map {|p|  p.organization_type_name || 'Unset'}
-  end
-
-  def role_name
-    participating_organizations.map {|p|  p.role_name || 'Unset'}
-  end
-
-  def organization_name
-    participating_organizations.map { |p|  p.organization_name || 'Unset'}
-  end
 
   # These used to be done inside the search block, now that FACETS are integrated, 
   # I had to move the code down here.
@@ -460,85 +390,11 @@ class Project < ActiveRecord::Base
     comments.present? ? "Has Comments" : nil
   end
 
-  def recipient_iso2
-    self.geopoliticals.map { |g| g.recipient_iso2 || "Unset" }
-  end
 
 
 
-  searchable do 
 
-    integer :id # for sorting
-    double :usd_2009 # for sorting
-    string :title # for sorting
-    string :donor_name # for sorting
-    string :recipient_condensed # fir sorting
 
-    # for text search:
-    text :id
-    text :title
-    text :description
-    text :capacity
-    text :sector_comment
-    text :year
-    text :donor_name
-    text :comments do 
-      comments.map do |c|
-        ["#{c.name}",
-          "#{c.content}"]
-      end
-    end
-    text :geopoliticals do
-      geopoliticals.map do |g| 
-        if g
-          ["#{g.subnational}",
-            "#{g.recipient_name }",
-            "#{g.recipient_iso3 }"]
-        end
-      end
-    end
-
-    text :participating_organizations do
-      participating_organizations.map do |o| 
-        ["#{o.organization_name }",
-          "#{o.role_name }",
-          "#{o.organization_type_name}"] 
-      end
-    end
-
-    text :sources do
-      sources.map do |s| 
-        ["#{s.url}",
-          "#{s.source_type_name}",
-          "#{s.document_type_name}",
-          "#{s.date ? s.date.strftime('%d %B %Y') : ''}",
-          "#{s.url.split(/\.|\/|\+|\%20|_/)}"]
-      end
-    end
-
-    text :transactions do
-      transactions.map do |t| 
-        ["#{t.currency_name}",
-          "#{t.value}"]
-      end
-    end
-
-    text :contacts do
-      contacts.map do |c| 
-        ["#{c.name}",
-          "#{c.position}",
-          "#{c.organization_name}"]
-      end
-    end
- 
-    # All the facets are defined in initializers/search_constants
-    (FACETS + WORKFLOW_FACETS).each do |facet|
-      string facet[:sym], multiple: (facet[:multiple] || false ) do 
-        facet[:code] || self.send(facet[:sym]) 
-      end
-    end
-
-  end
 
 	
 	def scope
@@ -593,16 +449,6 @@ class Project < ActiveRecord::Base
         {participating_organizations: {only: [], include: [{origin: {only: [:name]}}, {organization: {only: [:name, :organization_type]}}, {role: {only: [:name]}}]}}
     ]) 
   end
-  # I'll leave this here just in case.
-  # def history
-  #   [self.versions \
-  #     + self.transactions.map(&:versions) \
-  #     + self.sources.map(&:versions) \
-  #     + self.contacts.map(&:versions) \
-  #     + self.comments.map(&:versions) \
-  #     + self.participating_organizations.map(&:versions) \
-  #     + self.geopoliticals.map(&:versions)].flatten
-  # end
 
   def all_flags
     [
