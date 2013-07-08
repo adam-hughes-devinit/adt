f1fb0c42646628b099b82e488f6ccc4c38f36991
rmosolgo <rdmosolgo@gmail.com>
2013-07-08 17:09:58 -0400

    Adds unit tests and feature tests for Organization

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe Organization do
 	let(:organization) {FactoryGirl.create(:organization)}
+	let(:project) {FactoryGirl.build :project}
+	let(:user) {FactoryGirl.build :user}
 
 	subject {organization}
 
@@ -9,5 +11,44 @@ describe Organization do
 		it { should be_valid}
 	end
 
-	# describe "have proper deflators"
+	describe "can participate in projects" do
+		
+		before do 
+			p_o = ParticipatingOrganization.new(organization: organization)
+			project.participating_organizations << p_o
+			project.save!
+		end
+
+		it "should be in the project's organizations" do
+			project.participating_organizations.map(&:organization_id).should include(organization.id)
+		end
+
+		its(:projects){should include(project)}
+	end
+
+	describe "can own projects" do
+		before do
+			project.owner = organization
+			project.save!
+		end
+
+		it "should be the project's owner" do
+			project.owner.should eql(organization)
+		end
+
+		its(:owned_projects) {should include(project)}
+
+	end
+
+	describe "can own users" do
+		before do 
+			user.owner = organization
+			user.save!
+		end
+
+		its(:users){should include(user)}
+	end
+
+
+
 end
