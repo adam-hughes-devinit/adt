7bb1f11bd78ff0bf4bdae7d7152f11abf763f130
rmosolgo <rdmosolgo@gmail.com>
2013-07-09 15:45:19 -0400

    Stub out config vars in travis

--- app/mailers/export_mailer.rb
+++ app/mailers/export_mailer.rb
@@ -28,14 +28,9 @@ class ExportMailer < ActionMailer::Base
 
     export_file = File.open(export_file_name, 'w')
 
-    # add is_x scope variable for users to search by scope after exporting
     header = Project.csv_header
 
-    # MOVED TO lib/project_exporter_headers.rb -- now included in CSV HEADER
-    #  
-    # Scope.all.each do |scope|
-    #   header << ", is_scope_#{scope.symbol}?"
-    # end
+
     export_file.puts header
 
     #This is to update the progress bar on the export page
@@ -46,11 +41,7 @@ class ExportMailer < ActionMailer::Base
 
     @export.projects.each_with_index do |project, index|
       text =  project.csv_text
-      # MOVED TO lib/project_exporters.rb -- this is included in CSV TEXT
-      # # include the boolean value for is_x scope variable from above
-      # # Scope.all.each do |scope|
-      #   # text << ", #{project.scope.include?(scope.symbol.to_sym)}"
-      # # end
+
       export_file.puts text
 
       if index % 10 == 0
@@ -72,7 +63,7 @@ class ExportMailer < ActionMailer::Base
 
     # s3_upload(ENV['EXPORT_BUCKET'], export_file, "#{filename}.csv")
 
-    #mail it
+    # mail it
     mail.attachments["#{filename}.csv"] = {mime_type: 'text/csv',
                                               content: File.read(export_file_name)}
     mail( to: email, subject: "Your AidData export is ready!")
