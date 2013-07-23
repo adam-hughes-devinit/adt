50a4700567c149bb6fddbf02c92a46bfcbf8712a
adamfrey <adamparkerfrey@gmail.com>
2013-07-23 14:28:14 -0400

    Recent Changes

--- app/models/project_association_change.rb
+++ app/models/project_association_change.rb
@@ -27,20 +27,33 @@ class ProjectAssociationChange < ActiveRecord::Base
 
 
       first = Rails.cache.fetch("recent/first")
-      second = Rails.cache.fetch("recent/second")
 
-      Rails.cache.write("recent/third", second)
-      Rails.cache.write("recent/second", first)
-      @update_sent = "updated #{(attribute_name || "#{association_model}s").titleize.downcase}"
-      json = {
-        id: @project.id,
-        title: @project.title,
-        info: @project.to_english(exclude_title: true),
-        action: @update_sent,
-        time: self.updated_at
-      }
+      # if the last project was the same as this one, store it in the string
+      # instead of making a new entry
+      if first[:id] == project_id
+        first[:action] =
+          first[:action] << ", #{(attribute_name || "#{association_model}s").titleize.downcase}"
 
-      Rails.cache.write("recent/first", json)
+        first[:time] = self.updated_at
+        Rails.cache.write("recent/first", first)
+
+      else
+        # add a new entry and move the old ones down
+        second = Rails.cache.fetch("recent/second")
+
+        Rails.cache.write("recent/third", second)
+        Rails.cache.write("recent/second", first)
+        @update_sent = "updated #{(attribute_name || "#{association_model}s").titleize.downcase}"
+        json = {
+          id: @project.id,
+          title: @project.title,
+          info: @project.to_english(exclude_title: true),
+          action: @update_sent,
+          time: self.updated_at
+        }
+
+        Rails.cache.write("recent/first", json)
+      end
     else
       false
     end
