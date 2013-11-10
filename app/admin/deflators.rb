5cc94bca05f26822d2720fc3d251a52b34c97e1f
Alex Atkins <alex04634@gmail.com>
2013-11-10 17:32:54 -0500

    these admin pages now show belongs_to relationships

--- app/admin/deflators.rb
+++ app/admin/deflators.rb
@@ -1,3 +1,11 @@
 ActiveAdmin.register Deflator do
-  
+  index do
+    column :id
+    column :country
+    column :year
+    column :value
+    column :created_at
+    column :updated_at
+    default_actions
+  end
 end
--- app/admin/exchange_rates.rb
+++ app/admin/exchange_rates.rb
@@ -1,3 +1,13 @@
 ActiveAdmin.register ExchangeRate do
+  index do
+    column :id
+    column :from_currency
+    column :to_currency
+    column :year
+    column :rate
+    column :created_at
+    column :updated_at
+    default_actions
+  end
 
 end
