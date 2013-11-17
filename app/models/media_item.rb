7ce5ef78efe721dbff0a477d1b1965ff4edf38bb
Alex Atkins <alex04634@gmail.com>
2013-11-16 22:42:40 -0500

    fixed alignment and resizining in file viewer

--- app/models/media_item.rb
+++ app/models/media_item.rb
@@ -4,6 +4,6 @@ class MediaItem < ActiveRecord::Base
   attr_accessible :project_id, :publish, :media, :user_id, :downloadable, :url, :embed_code,
   :media_file_name, :media_content_type, :media_file_size, :media_updated_at
 
-  has_attached_file :media, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+  has_attached_file :media, :styles => { :large => "540x500>", :medium => "300x300>", :thumb => "100x100>" }
 
 end
