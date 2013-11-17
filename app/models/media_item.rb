060685f0d71bfe55f41289ede19821102a1fdcd3
Alex Atkins <alex04634@gmail.com>
2013-11-17 16:21:05 -0500

    formatting popups for file viewer

--- app/models/media_item.rb
+++ app/models/media_item.rb
@@ -1,7 +1,7 @@
 class MediaItem < ActiveRecord::Base
   belongs_to :project
   belongs_to :user
-  attr_accessible :project_id, :publish, :media, :user_id, :downloadable, :url, :embed_code,
+  attr_accessible :project_id, :publish, :media, :user_id, :downloadable, :url, :embed_code, :media_type,
   :media_file_name, :media_content_type, :media_file_size, :media_updated_at
 
   has_attached_file :media, :styles => { :large => "540x500>", :medium => "300x300>", :thumb => "100x100>" }
