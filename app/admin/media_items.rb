ae3a963a9054093f32aaff07a1f9fdcfb9066b7a
Alex Atkins <alex04634@gmail.com>
2013-11-18 11:29:56 -0500

    admin displays images and links to pdfs

--- app/admin/media_items.rb
+++ app/admin/media_items.rb
@@ -3,7 +3,15 @@ ActiveAdmin.register MediaItem do
     column :id
     column :project
     column "Image" do |media_item|
-      link_to(image_tag(media_item.media.url(:thumb), :height => '100'), admin_media_item_path(media_item))
+      if media_item.media_content_type == 'image/png'
+        link_to(image_tag(media_item.media.url(:thumb), :height => '100'), admin_media_item_path(media_item))
+      elsif media_item.media_content_type == 'image/jpeg'
+        link_to(image_tag(media_item.media.url(:thumb), :height => '100'), admin_media_item_path(media_item))
+      elsif media_item.media_content_type == 'image/gif'
+        link_to(image_tag(media_item.media.url(:thumb), :height => '100'), admin_media_item_path(media_item))
+      elsif media_item.media_content_type == 'application/pdf'
+        link_to(media_item.media_file_name, media_item.media.url)
+      end
     end
     column :media_file_name
 
