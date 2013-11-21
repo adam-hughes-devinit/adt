536c96871e8a49751191f45628e9224e30010698
Alex Atkins <alex04634@gmail.com>
2013-11-21 13:56:54 -0500

    fixed a bug that caused an error if project was nil

--- app/models/project_association_change.rb
+++ app/models/project_association_change.rb
@@ -23,45 +23,47 @@ class ProjectAssociationChange < ActiveRecord::Base
   # recent changes.
   def cache_change
     @project = Project.find_by_id(project_id)
-    if @project.is_stage_one == "Is not Stage One" && @project.active == true && attribute_name != 'active'
+    if !@project.nil?  # prevents issue where @project was returning null, if the project hadn't been approved yet.
+      if @project.is_stage_one == "Is not Stage One" && @project.active == true && attribute_name != 'active'
 
 
-      first = Rails.cache.fetch("recent/first")
+        first = Rails.cache.fetch("recent/first")
 
-      # if the last project was the same as this one, store it in the string
-      # instead of making a new entry
-      if first && first[:id] == project_id
-        action = first[:action]
-        new_action = "#{(attribute_name || "#{association_model}s").titleize.downcase}"
+        # if the last project was the same as this one, store it in the string
+        # instead of making a new entry
+        if first && first[:id] == project_id
+          action = first[:action]
+          new_action = "#{(attribute_name || "#{association_model}s").titleize.downcase}"
 
-        #no duplicates
-        unless /#{new_action}/ =~ action
-          first[:action] = action << ", #{new_action}"
-          first[:time] = self.updated_at
-          Rails.cache.write("recent/first", first)
+          #no duplicates
+          unless /#{new_action}/ =~ action
+            first[:action] = action << ", #{new_action}"
+            first[:time] = self.updated_at
+            Rails.cache.write("recent/first", first)
+          else
+            false
+          end
+
         else
-          false
+          # add a new entry and move the old ones down
+          second = Rails.cache.fetch("recent/second")
+
+          Rails.cache.write("recent/third", second)
+          Rails.cache.write("recent/second", first)
+          @update_sent = "updated #{(attribute_name || "#{association_model}s").titleize.downcase}"
+          json = {
+            id: @project.id,
+            title: @project.title,
+            info: @project.to_english(exclude_title: true),
+            action: @update_sent,
+            time: self.updated_at
+          }
+
+          Rails.cache.write("recent/first", json)
         end
-
       else
-        # add a new entry and move the old ones down
-        second = Rails.cache.fetch("recent/second")
-
-        Rails.cache.write("recent/third", second)
-        Rails.cache.write("recent/second", first)
-        @update_sent = "updated #{(attribute_name || "#{association_model}s").titleize.downcase}"
-        json = {
-          id: @project.id,
-          title: @project.title,
-          info: @project.to_english(exclude_title: true),
-          action: @update_sent,
-          time: self.updated_at
-        }
-
-        Rails.cache.write("recent/first", json)
+        false
       end
-    else
-      false
     end
   end
 
