7e5e0164b09d737ab090481a825a64e698d67d9f
Alex Atkins <alex04634@gmail.com>
2013-12-01 21:35:20 -0500

    changed message

--- app/models/homepage_media_item.rb
+++ app/models/homepage_media_item.rb
@@ -7,7 +7,7 @@ class HomepageMediaItem < ActiveRecord::Base
   validates_presence_of :url, :unless => :home_media?
   validates_presence_of :home_media, :unless => :url?
   validates_presence_of :order, :if => :published
-  validates :url, :format => { :with =>  /^(http|https):\/\/www\.youtube\.com\/watch\?v=([a-zA-Z0-9_-]*)/, :message => "Must be youtube url" }, :allow_blank => true
+  validates :url, :format => { :with =>  /^(http|https):\/\/www\.youtube\.com\/watch\?v=([a-zA-Z0-9_-]*)/, :message => "Whoa there buddy. Must be a valid youtube url." }, :allow_blank => true
 
   def youtube_embed(youtube_url,height,width,iframe_id)
     if youtube_url[/youtu\.be\/([^\?]*)/]
