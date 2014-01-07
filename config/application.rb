de75365b0936f2bca54f47308af960eb172c7b2c
Alex Atkins <alex04634@gmail.com>
2014-01-07 18:51:50 -0500

    added some comments

--- config/application.rb
+++ config/application.rb
@@ -38,7 +38,7 @@ module Adt
     # Activate observers that should always be running.
     # config.active_record.observers = :cacher, :garbage_collector, :forum_observer
     
-    config.cache_store = :memory_store
+    config.cache_store = :dalli_store
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
      config.time_zone = 'Eastern Time (US & Canada)'
