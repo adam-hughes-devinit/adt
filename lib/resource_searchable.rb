9fa4681f48a9131f7dedbc66b371caf1035e60cc
Alex Atkins <alex04634@gmail.com>
2014-01-14 18:33:39 -0500

    fixed issue with resource ordering

--- lib/resource_searchable.rb
+++ lib/resource_searchable.rb
@@ -15,13 +15,14 @@ module ResourceSearchable
 		searchable do 
 				text :title, :authors, :publisher_location, 
 				:publisher, :source_url, :id, :resource_type,
-				:url_searchable, :project_titles, :language_id
+				:url_searchable, :project_titles
 				string :resource_type
 				string :project_ids, multiple: true
 				string :publisher
 				string :title
         integer :language_id
 				integer :projects_count
+        time :fetched_at
 		end
 		
         # Nice idea, but I need it to show up in the search bar ~pronto~
