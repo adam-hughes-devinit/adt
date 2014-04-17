052b4e5bf68892b91b40daf87474d0e63083f7cb
Alex Atkins <alex04634@gmail.com>
2014-04-17 14:00:36 -0400

    better error logs for geo uploads

--- app/admin/geo_uploads.rb
+++ app/admin/geo_uploads.rb
@@ -41,7 +41,12 @@ ActiveAdmin.register GeoUpload do
     geo_upload = GeoUpload.create(csv: params[:dump][:file], record_count: 0, log_errors: 0)
     log_path = Rails.root.join('tmp')
     logfile = Tempfile.new('geo_upload.log', log_path)
-    logfile.write("Logfile\n")
+    logfile.write("Geo Uploads Logfile:\n")
+    record_stats = {"record_total" => 0,
+                    "uploaded_record_count" => 0,
+                    "missing_adms" => 0,
+                    "deprecated_precisions" => 0,
+                    "duplicate_geocodes" => 0}
 
     SmarterCSV.process(params[:dump][:file].tempfile,
                            {:chunk_size => 100,
@@ -55,12 +60,20 @@ ActiveAdmin.register GeoUpload do
                                              :location_type => :location_type }
                            }
     ) do |chunk|
-
-      geo_upload.record_count = GeoUpload.csv_to_database(chunk, geo_upload, logfile)
+      # perhaps refactor to work on geo_upload object
+      record_stats = GeoUpload.csv_to_database(chunk, geo_upload, logfile, record_stats)
     end
+    geo_upload.record_count = record_stats["uploaded_record_count"]
+    geo_upload.log_errors = record_stats["missing_adms"] + record_stats["deprecated_precisions"] + record_stats["duplicate_geocodes"]
 
+    logfile.write("\nSummary Statistics:\n")
+    logfile.write("#{geo_upload.record_count} out of #{record_stats["record_total"]} records added to the database.\n")
+    logfile.write("#{record_stats["missing_adms"]} records could not find an adm or geometry.\n")
+    logfile.write("#{record_stats["deprecated_precisions"]} records have a deprecated precision code.\n")
+    logfile.write("#{record_stats["duplicate_geocodes"]} records have the same project_id and geo_name_id as existing records.\n")
     logfile.read # fixes bug that prevents log file text being saved.
     geo_upload.log = logfile
+
     geo_upload.save
     redirect_to :action => :index, :notice => "CSV imported successfully!"
   end
@@ -84,7 +97,7 @@ ActiveAdmin.register GeoUpload do
 
     actions defaults: false do |post|
       link_to 'Delete', admin_geo_upload_path(post), :method => :delete,
-              :confirm => 'WARNING: Deleting this record will delete all geocodes from this upload!'
+              :confirm => 'WARNING: Deleting this record will delete all geocodes generated from this upload!'
     end
   end
   
--- app/models/geo_upload.rb
+++ app/models/geo_upload.rb
@@ -9,14 +9,14 @@ class GeoUpload < ActiveRecord::Base
 
   has_many :geocodes
 
-  def self.find_adm(lonlat, adm_level, logfile,  geocode, geo_upload)
+  def self.find_adm(lonlat, adm_level, logfile, geocode, record_stats)
     adm = Adm.where{level == adm_level}.joins{geometry}.where{st_contains(st_collectionextract(geometries.the_geom,3), lonlat)}.first
     if !adm.nil? # prevents crash if no adm match is found. Having world adms should fix this.
       geocode[:adm_id] = adm.id
       geocode[:geometry_id] = adm.geometry.id
     else
       logfile.write("Warning: geocode_id #{geocode.id}: Could not find Adm\n")
-      geo_upload.log_errors += 1
+      record_stats["missing_adms"] += 1
     end
 
     return geocode
@@ -37,7 +37,6 @@ class GeoUpload < ActiveRecord::Base
     geocode[:geo_name_id] = geo_name.id
 
     return geocode
-
   end
 
   # TODO: If geocodes have no adms for non-precision 1's and 2's, find nearest adm and use that.
@@ -50,11 +49,12 @@ class GeoUpload < ActiveRecord::Base
   ## No precision code, location_type, etc. Currently no nulls.
   ## Update Log time Watch Duplicate Copy Move Delete
 
-  def self.csv_to_database(chunk, geo_upload, logfile)
+  def self.csv_to_database(chunk, geo_upload, logfile, record_stats)
     chunk.each do |record|
+      record_stats["record_total"] += 1
       existing_geocode = Geocode.joins(:geo_name).where(project_id: record[:project_id], geo_name: {code: record[:geo_name_id]} ).first
       if existing_geocode.nil?
-        geo_upload.record_count += 1
+        record_stats["uploaded_record_count"] += 1
         geocode = Geocode.create
 
         geocode[:precision_id] = record[:precision_id].round
@@ -93,29 +93,29 @@ class GeoUpload < ActiveRecord::Base
           geocode[:geometry_id] = new_geometry.id
 
         elsif record[:precision_id] == 3  # its an adm2
-          geocode = GeoUpload.find_adm(lonlat, 2, logfile, geocode, geo_upload)
+          geocode = GeoUpload.find_adm(lonlat, 2, logfile, geocode, record_stats)
 
         elsif record[:precision_id] == 4  # its an adm1
-          geocode = GeoUpload.find_adm(lonlat, 1, logfile, geocode, geo_upload)
+          geocode = GeoUpload.find_adm(lonlat, 1, logfile, geocode, record_stats)
 
         elsif record[:precision_id] == 6 # its an adm0
-          geocode = GeoUpload.find_adm(lonlat, 0, logfile, geocode, geo_upload)
+          geocode = GeoUpload.find_adm(lonlat, 0, logfile, geocode, record_stats)
 
         elsif record[:precision_id] == 8  # its an adm0
-          geocode = GeoUpload.find_adm(lonlat, 0, logfile, geocode, geo_upload)
+          geocode = GeoUpload.find_adm(lonlat, 0, logfile, geocode, record_stats)
         else
            # It's a [5,7,9] precision code. Put it in the database, but doesn't get a geometry.
           logfile.write("Info: geocode_id #{geocode.id}: Deprecated precision code\n")
-          geo_upload.log_errors += 1
+          record_stats["deprecated_precisions"] += 1
         end
 
         geocode.save
       else
         logfile.write("Info: geocode_id #{existing_geocode.id}: This geocode already exists\n")
-        geo_upload.log_errors += 1
+        record_stats["duplicate_geocodes"] += 1
       end
     end
 
-    return geo_upload.record_count
+    return record_stats
   end
 end
