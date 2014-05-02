ded3cf5d92a28e5eee4efb4fa851f3ca4e4d1aa8
Alex Atkins <alex04634@gmail.com>
2014-05-02 12:43:38 -0400

    added later

--- db/migrate/20140325220907_create_geometries.rb
+++ db/migrate/20140325220907_create_geometries.rb
@@ -2,7 +2,6 @@ class CreateGeometries < ActiveRecord::Migration
   def change
     create_table :geometries do |t|
       t.geometry_collection :the_geom, :srid => 4326
-      t.integer :adm_code
 
       t.timestamps
     end
