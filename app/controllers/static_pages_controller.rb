49d60075192a7fa2721b8fb5b677d288d276f003
Alex Miller <alex.k.miller@gmail.com>
2014-06-17 09:20:30 -0400

    Moved AJAX script to helper, removed @ from local vars

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,7 @@
 class StaticPagesController < ApplicationController
   skip_before_filter :signed_in_user
   include SearchHelper
+  include GeospatialSearchHelper
   include AggregatesHelper
   require 'will_paginate/array'
   def home
@@ -67,19 +68,19 @@ class StaticPagesController < ApplicationController
   end
 
   def geospatial_dashboard
-    @file = File.read("public/dashboard_geojson.json")
-    @feature_collection = JSON.parse(@file)
+    file = File.read("public/dashboard_geojson.json")
+    @feature_collection = JSON.parse(file)
     #@feature_collection = Rails.cache.fetch("dashboard_geojson")
-    @search = Project.solr_search do
+    search = Project.solr_search do
       with :active_string, 'Active'
       paginate :page => 1, :per_page => 5
       order_by(:title,:asc)
     end
     @first_page = {}
-    @first_page["data"] = @search.results
-    @first_page["current"] = @search.results.current_page
-    @first_page["entries"] = @search.results.total_entries
-    @first_page["pages"] = @search.results.total_pages
+    @first_page["data"] = search.results
+    @first_page["current"] = search.results.current_page
+    @first_page["entries"] = search.results.total_entries
+    @first_page["pages"] = search.results.total_pages
     respond_to do |format|
       format.html { render 'geospatial_dashboard' }
       format.geojson { render json: @feature_collection }
@@ -88,122 +89,11 @@ class StaticPagesController < ApplicationController
   end
 
   def geospatial_search
-    @file = File.read("public/dashboard_geojson.json")
-    @feature_collection = JSON.parse(@file)
-    if params["search"]!=""
-      if params["search"].scan(/(?:\(.*?\))+/)[0].nil? || params["search"].scan(/(?:\(.*?\))+/)[0] =="(keyword)"
-        @search = Project.solr_search do
-          keywords params["search"].split(/(?:\(.*?\))+/)[0] do
-            fields(:description, :title => 2.0)
-          end
-          with :active_string, 'Active'
-        end
-        @full_result_ids = @search.results.map(&:id)
-        unless @feature_collection.nil?
-          @i = 0
-          while @i < @feature_collection["features"].length do
-            unless @full_result_ids.include? @feature_collection["features"][@i]["properties"]["project_id"]
-              @feature_collection["features"].delete_at(@i)
-            else
-              @i += 1
-            end
-          end
-          render :json => @feature_collection
-        end
-      elsif params["search"].scan(/(?:\(.*?\))+/)[0].start_with?("(ADM")
-        @search = Adm.solr_search do
-          keywords params["search"].split(/(?:\(.*?\))+/)[0] do
-            fields(:name)
-          end
-          paginate :page => 1, :per_page => 10000
-          with :level, params["search"].scan(/(?:\(.*?\))+/)[0].split(/(\d+)/)[1] || [0,1,2]
-        end
-        @geocodes = []
-        @geocodes.concat(@search.results.map(&:geocodes).flatten.map(&:id))
-        @geocodes.concat(@search.results.map(&:children).flatten.map(&:geocodes).flatten.map(&:id))
-        @geocodes.concat(@search.results.map(&:children).flatten.map(&:children).flatten.map(&:geocodes).flatten.map(&:id))
-        unless @feature_collection.nil?
-          @i = 0
-          while @i < @feature_collection["features"].length do
-            unless @geocodes.include? @feature_collection["features"][@i]["properties"]["geo_code_id"]
-              @feature_collection["features"].delete_at(@i)
-            else
-              @i += 1
-            end
-          end
-          render :json => @feature_collection
-        end
-      elsif params["search"].scan(/(?:\(.*?\))+/)[0].start_with?("(GEO")
-        @searchGeoName = Geocode.solr_search do
-          keywords params["search"].split(/(?:\(.*?\))+/)[0] do
-            fields(:geo_name)
-          end
-          paginate :page => 1, :per_page => 10000
-        end
-        @geocodes = []
-        @geocodes.concat(@searchGeoName.results.map(&:id))
-        unless @feature_collection.nil?
-          @i = 0
-          while @i < @feature_collection["features"].length do
-            unless @geocodes.include? @feature_collection["features"][@i]["properties"]["geo_code_id"]
-              @feature_collection["features"].delete_at(@i)
-            else
-              @i += 1
-            end
-          end
-          render :json => @feature_collection
-        end
-      else
-        @search = Project.solr_search do
-          keywords params["search"].split(/(?:\(.*?\))+/)[0] do
-            fields(:description, :title => 2.0)
-          end
-          with :active_string, 'Active'
-        end
-        @full_result_ids = @search.results.map(&:id)
-        unless @feature_collection.nil?
-          @i = 0
-          while @i < @feature_collection["features"].length do
-            unless @full_result_ids.include? @feature_collection["features"][@i]["properties"]["project_id"]
-              @feature_collection["features"].delete_at(@i)
-            else
-              @i += 1
-            end
-          end
-          render :json => @feature_collection
-        end
-      end
-    else
-      render :json => @feature_collection
-    end
+    geospatial_search_ajax
   end
 
   def json_completion
-    @search = Adm.solr_search do
-      keywords params['keywords'].nil??nil:params['keywords']+' OR '+params['keywords']+'*' do
-        fields(:name)
-      end
-    end
-    @searchGeoName = GeoName.solr_search do
-      keywords params['keywords'].nil??nil:params['keywords']+' OR '+params['keywords']+'*' do
-        fields(:name)
-      end
-    end
-    @i = 0
-    @len = @searchGeoName.results.first(4).length
-    @bucket = []
-    @bucket << params['keywords'] + " (keyword)"
-    while @i < @len
-      @bucket << @searchGeoName.results[@i]["name"] + " (GEO: " + [@searchGeoName.results[@i]].map(&:location_type)[0]["name"] + ")"
-      @i += 1
-    end
-    @i = 0
-    @len = @search.results.first(5).length
-    while @i < @len
-      @bucket << @search.results[@i]["name"] + " (ADM" + @search.results[@i]["level"].to_s + ")"
-      @i += 1
-    end
-    render :json => @bucket.flatten
+    json_completion_ajax
   end
 
   def new_map
