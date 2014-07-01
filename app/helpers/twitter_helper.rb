e3f7f6bd59a06f3218138e114ced2a4ec3e8a4a4
Alex Miller <alex.k.miller@gmail.com>
2014-07-01 13:44:26 -0400

    Longs can go negative

--- app/helpers/twitter_helper.rb
+++ app/helpers/twitter_helper.rb
@@ -1,26 +1,11 @@
 module TwitterHelper
   def search_twitter_ajax
     #Settings for Twitter Gem
-    client = Twitter::REST::Client.new do |config|
-      config.consumer_key    = 'oops'
-      config.consumer_secret = 'oops'
+    client = Twitter::Streaming::Client.new do |config|
+
     end
-    @tweetBucket = []
-    client.search("fifa", :result_type => "recent").take(5000000).each do |tweet|
-      unless tweet.geo.nil?
-        tweetObj = {}
-        tweetObj["text"] = tweet.text
-        tweetObj["author"] = tweet.user.name
-        tweetObj["handle"] = tweet.user.screen_name
-        unless tweet.place.nil?
-          tweetObj["geoname"] = tweet.place.full_name
-        else
-          tweetObj["geoname"] = nil
-        end
-        tweetObj["latLng"] = tweet.geo.coordinates
-        @tweetBucket << tweetObj
-      end
+    client.filter(:locations => "-122.75,36.8,-121.75,37.8") do |tweet|
+      puts tweet.text
     end
-    render :json => @tweetBucket
   end
 end
